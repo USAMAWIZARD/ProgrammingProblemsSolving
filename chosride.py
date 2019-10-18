d9d345b7de802ba5fbbc156359141fde96988495
USAMA <47350008+USAMAWIZARD@users.noreply.github.com>
2019-10-18 15:50:21 +0530

    Update chosride.py

--- chosride.py
+++ chosride.py
@@ -1,3 +1,4 @@
+#https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 import numpy as np
 toachive=np.array([2,5,1,3,4])
 og=np.arange(1,len(toachive)+1)
